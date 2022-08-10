056ad5727a778bf694b943c8f1e3d7d13a7dd25f
grdharshini <gr.priyadharshini96@gmail.com>
2022-08-10 10:04:31 +0530

    First commit

---
+++ Whileloop.java
@@ -0,0 +1,29 @@
+public class Whileloop {
+    public static void main(String[] args) {
+//        int i=1;
+//        while(i<10) {
+//            System.out.println(i);
+//            i++;
+//        }
+//        break
+//        int i = 1;
+//        while (i < 10) {
+//            System.out.println(i);
+//            if (i == 4) {
+//                break;
+//            }
+//            i++;
+//        }
+//    }
+//}
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 5);
+    }
+}
+
+
+
---
+++ Whileloop1.java
@@ -0,0 +1,18 @@
+public class Whileloop1 {
+    public static void main(String[] args) {
+//        int i = 1;
+//        while (i < 6) {
+//            System.out.println("Hello world");
+//            i++;
+//        Example2
+//        for(int i=0; i<=100; i++){
+//            System.out.print(i+ " ");
+//            if(i==50) {
+//                break;
+        int i = 20;
+        while (i<=50){
+            System.out.println(i);
+            }
+            }
+        }
+    }
